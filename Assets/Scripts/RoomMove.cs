cc028d9f0bb17f5a0fb20ef60751cbb7f5495330
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-04-12 16:33:03 -0700

    Room Transfers complete

---
+++ Assets/Scripts/RoomMove.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMove : MonoBehaviour
+{
+    public Vector2 cameraChangeMax;
+    public Vector2 cameraChangeMin;
+    public Vector3 playerChange;
+    private CameraMovement cam;
+    private bool TriggeredOnce = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && TriggeredOnce == false)
+        {
+            cam.minPosition += cameraChangeMin;
+            cam.maxPosition += cameraChangeMax;
+            other.transform.position += playerChange;
+            TriggeredOnce = true;
+        }
+        else if (other.CompareTag("Player") && TriggeredOnce == true)
+        {
+            cam.minPosition -= cameraChangeMin;
+            cam.maxPosition -= cameraChangeMax;
+            other.transform.position -= playerChange;
+            TriggeredOnce = false;
+        }
+    }
+
+}
