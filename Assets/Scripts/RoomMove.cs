a6b69d7e093c8adac90ba1cd16a9a8d065b66ef5
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-04-12 21:41:34 -0700

    Location Cards Added

--- Assets/Scripts/RoomMove.cs
+++ Assets/Scripts/RoomMove.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class RoomMove : MonoBehaviour
 {
@@ -8,6 +10,11 @@ public class RoomMove : MonoBehaviour
     public Vector2 cameraChangeMin;
     private CameraMovement cam;
     private bool TriggeredOnce = false;
+    public bool needText;
+    public string locationName1;
+    public string locationName2;
+    public GameObject text;
+    public TextMeshProUGUI placeText;
 
     // Start is called before the first frame update
     void Start()
@@ -28,13 +35,30 @@ public class RoomMove : MonoBehaviour
             cam.minPosition += cameraChangeMin;
             cam.maxPosition += cameraChangeMax;
             TriggeredOnce = true;
+            if (needText)
+            {
+                StartCoroutine(placeNameCo(locationName2));
+            }
         }
         else if (other.CompareTag("Player") && TriggeredOnce == true)
         {
             cam.minPosition -= cameraChangeMin;
             cam.maxPosition -= cameraChangeMax;
             TriggeredOnce = false;
+            if (needText)
+            {
+                StartCoroutine(placeNameCo(locationName1));
+            }
         }
+    }
+
+    private IEnumerator placeNameCo(string name)
+    {
+        text.SetActive(true);
+        placeText.text = name;
+        yield return new WaitForSeconds(4f);
+        text.SetActive(false);
+
     }
 
 }
