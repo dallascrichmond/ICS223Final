c8af84699d695dc2176caf90550f5131f308d7c4
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-04-13 22:37:34 -0700

    Added Moving Flags

---
+++ Assets/Scripts/SmashObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmashObject : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Smash()
+    {
+        anim.SetBool("smash", true);
+        StartCoroutine(breakCo());
+    }
+
+    IEnumerator breakCo()
+    {
+        yield return new WaitForSeconds(.3f);
+        this.gameObject.SetActive(false);
+    }
+}
