4b7108e01bf094abedce6e8996e81432c9dd1fb1
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-04-14 15:27:09 -0700

    Log animations complete

--- Assets/Scripts/Log.cs
+++ Assets/Scripts/Log.cs
@@ -35,14 +35,48 @@ public class Log : Enemy
             {
                 Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
 
+                ChangeAnim(temp - transform.position);
                 myRigidBody.MovePosition(temp);
                 ChangeState(EnemyState.walk);
+                anim.SetBool("wakeUp", true);
             }
         }
-        //else
-        //{
-        //    ChangeState(EnemyState.idle);
-        //}
+        else if(Vector3.Distance(target.position, transform.position) > chaseRadius)
+        {
+            anim.SetBool("wakeUp", false);
+        }
+    }
+
+    private void SetAnimFloat(Vector2 setVector)
+    {
+        anim.SetFloat("moveX", setVector.x);
+        anim.SetFloat("moveY", setVector.y);
+    }
+
+    private void ChangeAnim(Vector2 direction)
+    {
+        if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            if(direction.x > 0)
+            {
+                SetAnimFloat(Vector2.right);
+            }
+            else if(direction.x < 0)
+            {
+                SetAnimFloat(Vector2.left);
+            }
+        }
+        else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+        {
+            if(direction.y > 0)
+            {
+                SetAnimFloat(Vector2.up);
+            }
+            else if(direction.y < 0)
+            {
+                SetAnimFloat(Vector2.down);
+            }
+        }
     }
 
     private void ChangeState(EnemyState newState)
