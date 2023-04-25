019d2a987bac007ae45b12d28b7c02bec6d30960
dallascrichmond <113049138+dallascrichmond@users.noreply.github.com>
2023-04-25 12:56:18 -0700

    Cinemachine Priority bug fixed

--- Assets/Scripts/Game/Room.cs
+++ Assets/Scripts/Game/Room.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Room : MonoBehaviour
 {
@@ -19,6 +20,7 @@ public class Room : MonoBehaviour
                 ChangeActivation(pot, true);
 
             virturalCamera.SetActive(true);
+            virturalCamera.GetComponent<CinemachineVirtualCamera>().Priority = 99;
         }
     }
 
